18b2da68269115bd6b756984eacfe6aaf5daa613
nohehf <64989428+nohehf@users.noreply.github.com>
2022-03-31 10:26:46 +0200

    Add files via upload

---
+++ KSSIM_CARREZ_HINNIGER/puissance_iteree.m
@@ -0,0 +1,90 @@
+clear variables;clc
+% tolerance relative minimum pour l'ecart entre deux iteration successives 
+% de la suite tendant vers la valeur propre dominante 
+% (si |lambda-lambda_old|/|lambda_old|<eps, l'algo a converge)
+eps = 1e-8;
+% nombre d iterations max pour atteindre la convergence 
+% (si i > kmax, l'algo finit)
+imax = 5000; 
+
+% Generation d une matrice rectangulaire aleatoire A de taille n x p.
+% On cherche le vecteur propre et la valeur propre dominants de AA^T puis
+% de A^TA
+n = 1500; p = 500;
+A = 5*randn(n,p);
+
+% AAt, AtA sont deux matrices carrees de tailles respectives (n x n) et 
+% (p x p). Elles sont appelees "equations normales" de la matrice A
+AAt = A*A'; AtA = A'*A;
+
+%% Methode de la puissance iteree pour la matrice AAt de taille nxn
+% Point de depart de l'algorithme de la puissance iteree : un vecteur
+% aleatoire, normalise
+x = ones(n,1); x = x/norm(x);
+
+cv = false;
+iv1 = 0;        % pour compter le nombre d'iterations effectuees
+t_v1 = cputime; % pour calculer le temps d execution de l'algo
+err1 = 0;       % indication que le calcul est satisfaisant
+                % on stoppe quand err1 < eps 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% CODER L'ALGORITHME DE LA PUISSANCE ITEREE POUR LA MATRICE AAt
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+lambda1 = x' * AAt * x;
+while(~cv)
+   iv1 = iv1 + 1;
+   mu = lambda1;
+   x = AAt * x;
+   x = x / norm(x);
+   lambda1 = x' * AAt * x;
+   err1 = abs(lambda1 - mu) / abs(mu);
+   cv = (err1 <= eps || iv1 >= imax);
+end
+t_v1 = cputime-t_v1; % t_version1 : temps d execution de l algo de la 
+                     % puissance iteree pour la matrice AAt
+
+%% Methode de la puissance iteree pour la matrice AtA de taille pxp
+% Point de depart de l'algorithme de la puissance iteree : un vecteur
+% aleatoire, normalise
+y = ones(p,1); 
+y = y/norm(y);
+
+cv = false;
+iv2 = 0;        % pour compter le nombre d iterations effectuees
+t_v2 = cputime; % pour calculer le temps d execution de l'algo
+err2 = 0;       % indication que le calcul est satisfaisant
+                % on stoppe quand err2 < eps 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% CODER L ALGORITHME DE LA PUISSANCE ITEREE POUR LA MATRICE AtA
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+lambda2 = y' * AtA * y;
+while(~cv)
+   iv2 = iv2 + 1;
+   mu = lambda2;
+   y = AtA * y;
+   y = y / norm(y);
+   lambda2 = y' * AtA * y;
+   err2 = abs(lambda2 - mu) / abs(mu);
+   cv = (err2 <= eps || iv2 >= imax);
+end
+t_v2 = cputime-t_v2;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% APRES VOUS ETRE ASSURE DE LA CONVERGENCE DES DEUX METHODES, AFFICHER 
+% L'ECART RELATIF ENTRE LES DEUX VALEURS PROPRES TROUVEES, ET LE TEMPS
+% MOYEN PAR ITERATION POUR CHACUNE DES DEUX METHODES. 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fprintf('Erreur pour la methode avec la grande matrice = %0.3e\n', err1);
+fprintf('Erreur pour la methode avec la petite matrice = %0.3e\n', err2);
+fprintf('Ecart relatif entre les deux valeurs propres trouvees = %1.2e\n', (err1 - err2)/(max(err1,err2)))
+fprintf('Temps pour une ite avec la grande matrice = %0.3e\n', t_v1/iv1);
+fprintf('Temps pour une ite avec la petite matrice = %0.3e\n', t_v2/iv2);
+
+D = eig(AAt);
+D = sort(D, 'descend');
+
+fprintf('\nValeur propre dominante (methode avec la grande matrice) = %0.3e\n', lambda1);
+fprintf('Valeur propre dominante (methode avec la petite matrice) = %0.3e\n', lambda2);
+fprintf('Valeur propre dominante (fonction eig) = %0.3e\n', D(1));
